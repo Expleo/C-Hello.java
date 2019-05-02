15c627ebaddf9cc9e27377f6a597685e49ffa5ce
Expleo <47428837+Expleo@users.noreply.github.com>
2019-05-02 15:52:15 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,15 @@
+public class Program {
+    public static void main(String[] args) {
+        int age = 2;
+        int money = 800;
+        
+        if (age > 18 && money > 500) 
+        {
+            System.out.println("Welcome!");
+        }
+        else 
+        {
+            System.out.println("Sendoff!");
+        }
+    }
+}
